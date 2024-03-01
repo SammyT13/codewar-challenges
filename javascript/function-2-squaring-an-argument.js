d44420748be670c9a8569b4d08b0b92ccd2c57ba
Sammy Torres <storresii1316@gmail.com>
2024-03-01 12:12:17 -0500

    completed function 2 squaring an argument

---
+++ javascript/function-2-squaring-an-argument.js
@@ -0,0 +1,13 @@
+// Function 2-squaring an argument
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Psuedocode:
+// Function Signature:
+// Input: Number (Integer)
+// Output: Number (Integer)
+
+// Strategy: create a function called square, that takes an argument (number)
+
+
+
+const square = num => num**2
