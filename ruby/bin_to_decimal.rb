29faddc942e645ac1b07e90d1268e9386d1fa84c
Sammy Torres <storresii1316@gmail.com>
2025-03-25 15:21:54 -0400

    added notes to bin to decimal rb kata

--- ruby/bin_to_decimal.rb
+++ ruby/bin_to_decimal.rb
@@ -6,11 +6,11 @@ def bin_to_dec str
   decimal = 0
 
   # binary to decimal
-    # multiply each digit starting  from the rightmost digit by the powers of 2 starting with 2^0 and increase the exponent by 1
+    # multiply each digit starting  from the rightmost (LSB) digit by the powers of 2 starting with 2^0 and increase the exponent by 1 as you move to the leftmost (MSB) digit
     # then sum each value
   
   # used above algorithm but reverse the string so the left most digit becomes my right most digit
-    # this allows for me to use the index as my exponent starting with 0 then increasing by 1
+    # this allows for me to use the index as my exponent starting with 0 then increasing by 1 as I iterate 
   # afterwards sum the value 
 
   str.reverse.split("").each_with_index do | v, i |
